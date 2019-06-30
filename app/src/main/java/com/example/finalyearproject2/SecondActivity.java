640a27fcf37dd68694e0d17696d290aac7940d22
kevin <kevinfvdr-wb13@student.tarc.edu.my>
2019-06-30 21:35:35 +0800

    Added Oxford Dictionary API but Still Can't Function

--- app/src/main/java/com/example/finalyearproject2/SecondActivity.java
+++ app/src/main/java/com/example/finalyearproject2/SecondActivity.java
@@ -7,10 +7,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button Dict;
+
+
 
     FirebaseAuth firebaseAuth;
     @Override
@@ -18,7 +24,19 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         firebaseAuth.getInstance();
+
+        Dict = (Button)findViewById(R.id.btnDictionary);
+
+       Dict.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startActivity(new Intent(SecondActivity.this,DictionaryActivity.class));
+           }
+       });
+
     }
+
+
 
     //links Menu Layout
     @Override
