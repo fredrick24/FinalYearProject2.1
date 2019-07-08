ee90f83f7d00cade394fcd1ed5248112a4170bfb
kevin <kevinfvdr-wb13@student.tarc.edu.my>
2019-07-08 20:47:43 +0800

    Added Text To Speech Function

--- app/src/main/java/com/example/finalyearproject2/DictionaryActivity.java
+++ app/src/main/java/com/example/finalyearproject2/DictionaryActivity.java
@@ -7,13 +7,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.speech.tts.TextToSpeech;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Locale;
 
 public class DictionaryActivity extends AppCompatActivity {
 
     private EditText edit1;
     private TextView D1;
-    private Button define;
+    private Button define,btnTTS;
+    private TextToSpeech tts1;
     String url;
 
 
@@ -24,11 +30,41 @@ public class DictionaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
 
-        edit1 =(EditText)findViewById(R.id.etWord);
-        D1 = (TextView)findViewById(R.id.tvDefine);
-        define = (Button)findViewById(R.id.btnDefine);
+        edit1 = (EditText) findViewById(R.id.etWord);
+        D1 = (TextView) findViewById(R.id.tvDefine);
+        define = (Button) findViewById(R.id.btnDefine);
+        btnTTS = (Button) findViewById(R.id.btnTTS);
 
+        tts1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    tts1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        btnTTS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String toSpeak = edit1.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+                tts1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
     }
+
+        public void onPause(){
+            if(tts1 != null){
+                tts1.stop();
+                tts1.shutdown();
+            }
+            super.onPause();
+        }
+
+
+
+
 
 
     public void requestApiButtonClick(View view){
